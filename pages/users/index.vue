5df52d8680c0c2cf875a26f8b5ca30ee5cb57aa0
97fyl97@gmail.com <97fyl97@gmail.com>
2020-06-29 16:15:15 +0300

    not work

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,27 +1,35 @@
 <template>
   <section>
-    <h1>Users Page</h1>
+    <h1>{{pageTitle}}</h1>
+
     <ul>
       <li v-for="user of users" :key="user.id">
-        <a href="#" @click.prevent="openUser(user)">{{ user.name }}</a>
+        <a href="#" @click.prevent="openUser(user)">{{user.name}}</a>
       </li>
     </ul>
   </section>
 </template>
+
 <script>
 export default {
+  async fetch({ store }) {
+    if (store.getters["users/users"].length === 0) {
+      await store.dispatch("users/fetch");
+    }
+  },
   data: () => ({
-    users: []
+    pageTitle: "Users page"
   }),
-  async mounted() {
-    this.users = await this.$axois.$get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
+  computed: {
+    users() {
+      return this.$store.getters["users/users"];
+    }
   },
   methods: {
     openUser(user) {
-      this.$router.push("/users/" + users);
+      this.$router.push("/users/" + user.id);
     }
   }
 };
 </script>
+
